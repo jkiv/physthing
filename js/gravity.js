ffa9a39a33eba19bb36d4616f9c27e6932fe5dc9
Jon Kivinen <jon@jkiv.ca>
2014-12-23 16:03:12 -0500

    Gravity working.

---
+++ js/gravity.js
@@ -0,0 +1,142 @@
+/**
+ * Handles gravity and body interactions.
+ */
+gthing.Gravity = function(G) {
+  this.G = G || 10.0;
+  this.bodies = [];
+}
+
+/**
+ * Compute and add gravitational forces for two interacting bodies [a] and [b].
+ */
+gthing.Gravity.prototype.applyBodyForces = function(a, b) {
+    var abVector = new THREE.Vector3();
+    abVector.subVectors(b.physics.position, a.physics.position); // from [a] to [b]
+    var rSq = abVector.lengthSq();
+    var forceMag = (rSq === 0) ? 0.0 : this.G * a.physics.mass * b.physics.mass / rSq;
+    var norm = abVector.clone().normalize();
+    
+    // Force in equal and opposite directions (thx @therealnewton)
+    a.physics.forces.push(norm.clone().multiplyScalar(forceMag));
+    b.physics.forces.push(norm.clone().multiplyScalar(-forceMag));
+}
+
+/**
+ * Determine whether two objects are within each other's interacting range.
+ */
+gthing.Gravity.prototype.inInteractionRange = function(a, b) {
+  var distance = a.physics.position.distanceTo(b.physics.position);
+  var maximumDistance = a.physics.gravity.interactionRadius
+                        + b.physics.gravity.interactionRadius;
+  
+  return distance <= maximumDistance;
+}
+
+gthing.Gravity.prototype.findInteractingBodies = function(bodies){
+  var visitedFrom = [];
+  var pairs = [];
+  var that = this;
+  
+  // Find interacting bodies the dumb way
+  // 1. for each body, test against all other bodies
+  // 2. skip bodies in inner loop that were seen in outer loop
+  _.forEach(bodies, function(bodyFrom) {
+    // Start remembering who we visited from [bodyFrom]
+    visitedFrom.push(bodyFrom);
+    
+    // Skip body if no gravity present
+    if (bodyFrom.physics.gravity === undefined) {
+      return;
+    }
+    
+    _.forEach(bodies, function(bodyTo) {
+      // Skip body if no gravity present
+      if (bodyTo.physics.gravity === undefined) {
+        return;
+      }
+      
+      // Have we seen this pair before?
+      if (_.contains(visitedFrom, bodyTo) === false) {
+        // We have not seen this pair before. Test interaction.
+        if (that.inInteractionRange(bodyTo, bodyFrom)) {
+           // Interacting pair, remember pair
+           pairs.push([bodyTo, bodyFrom]);
+        }
+      }
+    }); 
+  });
+  
+  return pairs;
+}
+
+/**
+ * Finds interacting bodies in [bodies] and applies gravitational force
+ * to each interacting pair.
+ * \see gthing.Gravity.findInteractingBodies
+ * \see gthing.Gravity.applyBodyForces
+ */
+gthing.Gravity.prototype.updateForces = function() {
+  var that = this;
+  _.forEach(this.findInteractingBodies(this.bodies), function(pair) {
+    that.applyBodyForces(pair[0], pair[1]);
+  });
+}
+
+/**
+ * Put [body] under the guise of this Gravity object.
+ */
+gthing.Gravity.prototype.add = function(body) {
+  this.bodies = _.union(this.bodies, [body]);
+}
+
+/**
+ * Remove [body] from the guise of this Gravity object.
+ */
+gthing.Gravity.prototype.remove = function(body) {
+  this.bodies = _.difference(this.bodies, [body]);
+}
+
+/**
+ * Gravity test scene (1).
+ */
+gthing.Gravity.testScene1 = function() {
+  // Add planet one -- like a Sun
+  var planet = new gthing.Planet(30, 10e3, 1e6);
+  gthing.entities.push(planet);  // tell game loop to handle this object
+  gthing.gravity.add(planet);    // tell gravity to handle this object
+  gthing.scene.add(planet.mesh); // put object in scene
+  
+  // ... make it like a sun
+  planet.mesh.material.color = new THREE.Color(0xffff00);
+  planet.mesh.material.emissive = new THREE.Color(0x606000);
+  planet.mesh.add(new THREE.PointLight(0xffffff, 0.5, 100));
+  
+  gthing.scene.remove(gthing.camera);
+  planet.mesh.add(gthing.camera);
+  
+  // Add planet two -- like a Planet
+  var planet = new gthing.Planet(10, 10, 1000);
+  planet.mesh.material.color = new THREE.Color(0x0000c0);
+  gthing.entities.push(planet);  // tell game loop to handle this object
+  gthing.gravity.add(planet);    // tell gravity to handle this object
+  gthing.scene.add(planet.mesh); // put object in scene
+  
+  planet.mesh.translateX(-100);
+  //planet.physics.position = planet.mesh.position;
+  planet.physics.velocity = new THREE.Vector3(0,-220,0);
+  gthing.gravity.add(planet);
+  
+  // Add planet three -- like a Moon
+  var m3 = 10;
+  var r3 = 100;
+  var planet = new gthing.Planet(4, m3, 1000);
+  planet.mesh.material.color = new THREE.Color(0xc0c0c0);
+  gthing.entities.push(planet);  // tell game loop to handle this object
+  gthing.gravity.add(planet);    // tell gravity to handle this object
+  gthing.scene.add(planet.mesh); // put object in scene
+  
+  planet.mesh.translateX(r3);
+  //planet.physics.position = planet.mesh.position;
+  planet.physics.velocity = new THREE.Vector3(0,30,0);
+  gthing.gravity.add(planet);
+}
--- js/gthing.js
+++ js/gthing.js
@@ -1,44 +1,115 @@
 // Requires: lodash.js, three.js, jquery.js
 
-// Create gthing namespace
-var gthing = {};
+// gthing Namespace
+var gthing = {
+  scene: null,
+  camera: null,
+  renderer: null,
+  entities: [],
+  gravity: null,
+  clock: null
+};
 
 // Main entry point
 gthing.go = function() {
-  // set the scene size
-  var WIDTH = 400,
-    HEIGHT = 300;
-
-  // set some camera attributes
-  var VIEW_ANGLE = 45,
-    ASPECT = WIDTH / HEIGHT,
-    NEAR = 0.1,
-    FAR = 10000;
-
-  // Create a WebGL renderer
-  var renderer = new THREE.WebGLRenderer();
+  // Initialize scene components
+  gthing.initalizeScene();
   
-  // Create a camera
-  var camera =
-    new THREE.PerspectiveCamera(
-      VIEW_ANGLE,
-      ASPECT,
-      NEAR,
-      FAR);
+  // ADD TEST PLANETS ////
+  gthing.gravity = new gthing.Gravity();
+  gthing.Gravity.testScene1();
+  ////////////////////////
+  
+  // Start game loop
+  gthing.loop();
+}
 
+// Render loop
+gthing.loop = function() {
+  // Get time delta since last frame
+  var timedelta = gthing.clock.getDelta() / 10;
+  timedelta = 1./60.; // fix timestep for consistent physics
+  
+  // Read input -- dispatch events
+  // TODO
+  
+  // Perform gravity
+  gthing.gravity.updateForces();
+  
+  // Perform collisions and constraints
+  // TODO collision system
+  
+  // Accumulate forces and update positions
+  _.forEach(gthing.entities, function(entity) {
+    entity.updatePosition(timedelta);
+  });
+  
+  // Render scene
+  requestAnimationFrame(gthing.loop); // using built-in browser animation API
+  gthing.renderScene();
+}
+
+// Load scene into gthing.threejs.scene
+gthing.initalizeScene = function() {
+  // Grab target DOM object
+  var container = $("#container");
+
+  // Create render target
+  var renderer = new THREE.WebGLRenderer({
+    antialias: true // ...might as well try
+  });
+  
+  // Create camera
+  var renderProperties = {
+    width: container.width(),
+    height: container.height(),
+    near: 0.1,
+    far: 10000
+  };
+
+  console.log(renderProperties);
+  
+  var camera =
+    new THREE.OrthographicCamera(
+      renderProperties.width / - 2,
+      renderProperties.width / 2,
+      renderProperties.height / 2,
+      renderProperties.height / - 2,
+      renderProperties.near,
+      renderProperties.far
+    );
+  
   // Create a scene
   var scene = new THREE.Scene();
 
-  // add the camera to the scene
+  // Add camera to scene
   scene.add(camera);
-
-  // the camera starts at 0,0,0
-  // so pull it back
   camera.position.z = 300;
 
-  // start the renderer
-  renderer.setSize(WIDTH, HEIGHT);
+  // Add ambient light to scene
+  //var ambientLight = new THREE.AmbientLight( 0x606060 );
+  var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.6 );
+  directionalLight.position.set(0,0,1);
+  //scene.add(ambientLight);
+  scene.add(directionalLight);
+  
+  // Put render target into page
+  renderer.setSize(renderProperties.width, renderProperties.height);
+  container.append(renderer.domElement);
+  
+  // Resize the renderer on resize of window/div
+  container.bind('resize', function(e){
+    // TODO resize renderer
+  });
+  
+  // Hold references for future use
+  gthing.scene = scene;
+  gthing.renderer = renderer;
+  gthing.camera = camera;
+  gthing.clock = new THREE.Clock(true);
+}
 
-  // attach the render-supplied DOM element
-  $("#container").append(renderer.domElement);
+// Render the scene using the current renderer, scene, and camera
+gthing.renderScene = function() {
+  gthing.renderer.render(gthing.scene, gthing.camera);
 }
